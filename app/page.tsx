8a497da331c6bede64501b86188853010542eb6b
neoqasim <neoqasim@gmail.com>
2024-08-14 01:28:49 +0500

    deleted extra files

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard"
 export default function Home() {
   return (
     <main>
       <h1>hi </h1>
-      <Link href="/user" >User Page</Link>
-      <ProductCard />
     </main>
   );
 }
